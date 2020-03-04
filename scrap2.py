41c8f3420cf06c40a66e30f78773ade5d395560e
Andrey Smetanin <smetanin@localhost.ru>
2020-03-04 16:48:28 +0400

    json fix

--- scrap2.py
+++ scrap2.py
@@ -1,4 +1,5 @@
 from twitterscraper.query import query_user_info
+from twitterscraper.query import query_tweets_from_user
 import pandas as pd
 from multiprocessing import Pool
 import time
@@ -7,7 +8,7 @@ import json
 import collections
 
 global twitter_user_info
-twitter_user_info=[]
+twitter_user_info = []
 
 
 def get_user_info(twitter_user):
@@ -16,7 +17,7 @@ def get_user_info(twitter_user):
     :param twitter_user: the twitter user to capture user data
     :return: twitter_user_data: returns a dictionary of twitter user data
     """
-    user_info = query_user_info(user= twitter_user)
+    user_info = query_user_info(user=twitter_user)
     twitter_user_data = {}
     twitter_user_data["user"] = user_info.user
     twitter_user_data["fullname"] = user_info.full_name
@@ -29,11 +30,12 @@ def get_user_info(twitter_user):
     twitter_user_data["followers"] = user_info.followers
     twitter_user_data["likes"] = user_info.likes
     twitter_user_data["lists"] = user_info.lists
-    
+
     return twitter_user_data
 
 
 def main():
+    '''
     start = time.time()
     users = ['realDonaldTrump']
 
@@ -49,23 +51,42 @@ def main():
     print("Elapsed time: ")
     print(elapsed)
     display(data_frame)
-data = {}   
-data['aa'] = "aa"
 
-json_dump = json.dumps(data)
-json_object = json.loads(json_dump)
+    for tweet in query_tweets_from_user("realDonaldTrump", limit=1):
+        data['aa'] = "aa"
+        data['screen_name'] = tweet.screen_name
+        data['timestamp'] = tweet.timestamp
+        data['timestamp_epochs'] = tweet.timestamp_epochs
+        data['text'] = tweet.text
+        #print(tweet.text)
+'''
 
-with open('data.json', 'w') as f:
-  json.dump(json_object, f, ensure_ascii=False, indent=2)
+    #data_array[0] = data
+    #count = 0
+
+
+    #data[count] = "aa"
+    json_object_array = []
+    data = {}
+    data['aa'] = "aa"
+    data['screen_name'] = "tweet.screen_name"
+    s = "fsdf"
+    for tweet in query_tweets_from_user("realDonaldTrump", limit=1):
+        #print(tweet.text)        
+        s = tweet.text
+        print(s)
+        json_dump = json.dumps(data)
+        json_object_array.append(json.loads(json_dump))
+    #for count in range(20):
+        #data[count] = "ss"
+        #json_dump = json.dumps(data)
+        #json_object_array.append(json.loads(json_dump))
+    #print(data)
+    with open('data.json', 'w') as f:
+        json.dump(json_object_array, f, ensure_ascii=False, indent=2)
 
 
 print("Done!")
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
